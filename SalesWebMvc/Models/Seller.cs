ed30bb774f6c4621a87f535b277e9a0b02e779af
deisemoraesf <deise.moraesfarias@gmail.com>
2021-11-23 18:58:13 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BithDate { get; set; }
+        public double BaseSalary { get; set; }
+
+        public Department Departament { get; set; }
+
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime bithDate, double baseSalary, Department departament)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BithDate = bithDate;
+            BaseSalary = baseSalary;
+            Departament = departament;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime Inicial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= Inicial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
